0788c1d9dea8d78866e723402c777fd96bdf9bcf
lxy <lxy2830@163.com>
2023-03-18 23:46:18 +0800

    思考了一些题目

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import torch.cuda
 
 if __name__ == '__main__':
     print(torch.cuda.is_available())
-
+    print(torch.cuda.device_count())
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
